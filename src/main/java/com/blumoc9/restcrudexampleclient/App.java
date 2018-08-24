ea1fac53b698f9721fb929ba7fc76455644776c2
Fernando Colmenarez <fcolmenarez@technisys.com>
2018-08-24 16:38:03 -0300

    Primer Commit del Cliente con Jersey

---
+++ src/main/java/com/blumoc9/restcrudexampleclient/App.java
@@ -0,0 +1,13 @@
+package com.blumoc9.restcrudexampleclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
