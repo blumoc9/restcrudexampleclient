77c1343816e5c1946864794ef70ec2da31a89124
Fernando Colmenarez <fcolmenarez@technisys.com>
2018-08-24 17:07:08 -0300

    modificacion de prueba

--- src/main/java/com/blumoc9/restcrudexampleclient/model/Employee.java
+++ src/main/java/com/blumoc9/restcrudexampleclient/model/Employee.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
  
 @XmlRootElement(name = "employee")
-@XmlAccessorType(XmlAccessType.FIELD)
+@XmlAccessorType(XmlAccessType.FIELD) 
 public class Employee {
  
   private String empNo;
